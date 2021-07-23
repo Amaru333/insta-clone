c1022569e5884b5189452415b04aed060c17746b
Amaru333 <amrutesharun0599@gmail.com>
2021-07-23 11:44:58 +0530

    Changes to view post card UI

--- src/components/PostView.js
+++ src/components/PostView.js
@@ -4,12 +4,17 @@ import "../styles/PostView.css";
 function PostView({ username, image, description }) {
   return (
     <div className="postView">
-      <p>{username}</p>
+      <p className="post_username">{username}</p>
       <img
         className="post_image_home"
         src={`http://localhost:3001/images/${image}`}
       />
-      <p>{description}</p>
+      <p className="post_contents">
+        <span className="post_username" style={{ padding: "0px" }}>
+          {username}
+        </span>
+        <span className="post_description">{description}</span>
+      </p>
     </div>
   );
 }
